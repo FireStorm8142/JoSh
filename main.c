90c7a9c88b2c2909ac63a5c4a713402cb400f02a
Johan Paul <johanjohn405@gmail.com>
2026-05-02 19:38:58 +0000

    Initial Commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  setbuf(stdout, NULL);
+
+  printf("$ ");
+  char command[1024];
+  fgets(command,sizeof(command) ,stdin);
+  command[strcspn(command, "\n")] = '\0';
+  printf("%s: command not found\n", command);
+
+  return 0;
+}
