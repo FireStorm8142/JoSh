22b78a9ff5058ba32b4ba3745a46a1999d9af9f0
Johan Paul <johanjohn405@gmail.com>
2026-05-03 18:01:14 +0000

    Created PATH env

--- builtins.c
+++ builtins.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "builtins.h"
 
-//Built in cmd for echo
 int cmd_echo(char **args) {
     for (int i = 1; args[i]; i++) {
         printf("%s", args[i]);
@@ -13,7 +13,6 @@ int cmd_echo(char **args) {
     return 0;
 }
 
-//Built in cmd for exit
 int cmd_exit(char **args) {
     exit(0);
 }
@@ -25,6 +24,27 @@ int cmd_type(char **args){
             printf("%s is a shell builtin\n", builtins[i].name);
             return 0;
         }
+    }
+
+    char *path = getenv("PATH");
+
+    if (path == NULL) return 0;
+
+    char path_copy[1024];
+    strcpy(path_copy, path);
+
+    char *dir = strtok(path_copy, ":");
+
+    while (dir != NULL) {
+        char full[1024];
+        snprintf(full, sizeof(full), "%s/%s", dir, args[1]);
+
+        if (access(full, X_OK) == 0) {
+            printf("%s is %s\n", args[1], full);
+            return 0;
+        }
+
+        dir = strtok(NULL, ":");
     }
     printf("%s: not found\n", args[1]);
     return 0;
