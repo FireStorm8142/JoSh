93ceb93080ce04e5b7a1920119e4276b0117f916
Johan Paul <johanjohn405@gmail.com>
2026-05-03 03:28:57 +0000

    Exit Command Implemented

--- main.c
+++ main.c
@@ -3,13 +3,18 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  setbuf(stdout, NULL);
+	setbuf(stdout, NULL);
 
-  printf("$ ");
-  char command[1024];
-  fgets(command,sizeof(command) ,stdin);
-  command[strcspn(command, "\n")] = '\0';
-  printf("%s: command not found\n", command);
+	char command[1024];
 
-  return 0;
+	while(1){
+		printf("$ ");
+		fgets(command,sizeof(command) ,stdin);
+		command[strcspn(command, "\n")] = '\0';
+
+		if (strcmp(command, "exit") == 0) break;
+
+		printf("%s: command not found\n", command);
+	}
+	return 0;
 }
